12446a49dc9f266da35964ecbe72e30e05f1336e
Vishakha Aggarwal <76397035+vishakha-aggarwal@users.noreply.github.com>
2022-09-11 23:37:24 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
 const app = require("./app");
-
-// const dotenv = require("dotenv");
 const cloudinary = require("cloudinary");
+const connectDatabase = require("./config/database");
 
-const connectDatabase = require("./config/database.js");
+// Handling Uncaught Exception
+process.on("uncaughtException", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to Uncaught Exception`);
+    process.exit(1);
+});
 
-if(process.env.NODE_ENV!=="production")
-    require("dotenv").config({path:"backend/config/config.env"});
+// Config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+    require("dotenv").config({ path: "backend/config/config.env" });
+}
 
+// Connecting to database
+connectDatabase();
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
@@ -15,33 +23,16 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-//handling uncaught exception
-process.on("uncaughtException", err =>{
-    console.log(err);
-    console.log("Shutting down server due to errors");
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Server is working on http://localhost:${process.env.PORT}`);
+});
+
+// Unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to Unhandled Promise Rejection`);
 
     server.close(() => {
         process.exit(1);
-    })
-})
-
-connectDatabase();
-
-
-app.get("/", (req, res) => {
-    res.json("server started");
-})
-
-const server = app.listen(process.env.PORT, (req, res) => {
-    console.log(`server started on port ${process.env.PORT}`);
-})
-
-//unhandled promise rejection
-process.on("unhandledRejection", err =>{
-    console.log(err);
-    console.log("Shutting down server due to errors");
-
-    server.close(() => {
-        process.exit(1);
-    })
-})
+    });
+});
